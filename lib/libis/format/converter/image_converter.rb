c6abe40f02b945dcb89ed749884c7be75ae62eea
Kris Dekeyser <kris.dekeyser@libis.be>
2016-07-28 14:16:11 +0200

    Version bump: 0.9.25 ImageConverter: resample operation now really resamples

--- lib/libis/format/converter/image_converter.rb
+++ lib/libis/format/converter/image_converter.rb
@@ -49,7 +49,7 @@ module Libis
         end
 
         def resample(value)
-          @options[:density] = value
+          @options[:resample] = value
         end
 
         def flatten
--- lib/libis/format/version.rb
+++ lib/libis/format/version.rb
@@ -1,5 +1,5 @@
 module Libis
   module Format
-    VERSION = '0.9.24'
+    VERSION = '0.9.25'
   end
 end
