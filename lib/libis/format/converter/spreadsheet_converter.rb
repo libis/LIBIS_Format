84b454c84ba1645c79a104e9015ac6ab4b938f3c
Kris Dekeyser <kris.dekeyser@libis.be>
2018-06-11 23:38:40 +0200

    Added spreadsheet converter

---
+++ lib/libis/format/converter/spreadsheet_converter.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+require_relative 'base'
+
+require 'libis/format/tool/spreadsheet_to_ods'
+require 'libis/format/type_database'
+
+module Libis
+  module Format
+    module Converter
+
+      class SpreadsheetConverter < Libis::Format::Converter::Base
+
+        def self.input_types
+          [
+              :MSXLS,
+              :MSXLSX,
+              :OO_CALC
+          ]
+        end
+
+        def self.output_types(format = nil)
+          return [] unless input_types.include?(format)
+          [:OO_CALC]
+        end
+
+        def spreadsheet_convert(_)
+          #force usage of this converter
+        end
+
+        def convert(source, target, format, opts = {})
+          super
+
+          return nil unless Format::Tool::SpreadsheetToOds.run(source, target)
+
+          target
+
+        end
+
+      end
+
+    end
+  end
+end
