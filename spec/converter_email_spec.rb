424550aa677518d809203ee41219f1514c652cba
Kris Dekeyser <kris.dekeyser@kuleuven.be>
2023-08-30 19:57:22 +0200

    Make tests work again

--- spec/converter_email_spec.rb
+++ spec/converter_email_spec.rb
@@ -22,13 +22,14 @@ describe "Converters" do
       tgt_file_truth = File.join(data_dir, "email", "test_msg.pdf")
       headers_file = File.join(work_dir, "test_msg.headers.xml")
       headers_file_truth = File.join(data_dir, "email", "test_msg.headers.yml")
-      attachments_file = File.join(work_dir, "test_msg-attachments", "test simple email.eml")
+      attachments_file = File.join(work_dir, "test_msg.pdf.attachments", "1-test simple email.msg.pdf")
       FileUtils.mkdir_p File.dirname(tgt_file)
       result = converter.convert(src_file, tgt_file, :PDF)
       expect(result[:files].size).to eq 2
       expect(result[:files]).to match_array([tgt_file, attachments_file])
       headers = YAML.load_file(headers_file_truth).symbolize_keys!
       expect(result[:headers]).to include(headers)
+      expect(result[:files][1..]).to match_array(headers[:attachments].map{|f| File.join(work_dir,f)})
       FileUtils.rm tgt_file, force: true
       FileUtils.rm headers_file, force: true
       FileUtils.rm attachments_file, force: true
